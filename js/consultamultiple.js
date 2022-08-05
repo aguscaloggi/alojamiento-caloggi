ffb34b135358c84225e03c5b1bc07c6f9d9aca79
aguscaloggi <agus.caloggi00@gmail.com>
2022-08-04 23:24:49 -0300

    librerias

--- js/consultamultiple.js
+++ js/consultamultiple.js
@@ -6,6 +6,7 @@ let salida = localStorage.getItem('salida');
 let mayores = localStorage.getItem('mayores');
 let menores = localStorage.getItem('menores');
 let consulta = sessionStorage.getItem('consulta');
+const DateTime = luxon.DateTime;
 
 document.getElementById('flname').value = flname;
 document.getElementById('email').value = email;
@@ -15,16 +16,17 @@ document.getElementById('mayores').value = mayores;
 document.getElementById('menores').value = menores;
 document.getElementById('consulta').value = consulta;
 
+// console.log(DateTime.fromJSDate(new Date (document.getElementById('entrada').value)));
     function enviarFormulario() {
-        let today = new Date();
-        let m = today.getMonth();
-        let d = today.getDay();
-        let y = today.getYear();
-        let fecha = (m,d,y);
+        // let today = new Date();
+        // let m = today.getMonth();
+        // let d = today.getDay();
+        // let y = today.getYear();
+        // let fecha = (m,d,y);
     
-        let entradaDate = new Date(entrada);
-        let salidaDate = new Date(salida);
-    
+        // let entradaDate = new Date(entrada);
+        // let salidaDate = new Date(salida);
+
         let flname = document.getElementById('flname').value;
         let email = document.getElementById('email').value;
         let entrada = document.getElementById('entrada').value;
@@ -33,27 +35,45 @@ document.getElementById('consulta').value = consulta;
         let menores = document.getElementById('menores').value;
         let consulta = document.getElementById('consulta').value;
 
-        if (fecha > entradaDate) {
-            alert("La consulta debe ser para una fecha posterior a la de hoy.");
+        let entradaLux = (DateTime.fromJSDate(new Date (entrada)));
+        let salidaLux = (DateTime.fromJSDate(new Date (salida)));
+        let fechaHoy = DateTime.local;
+
+        if (fechaHoy > entradaLux) {
+            Swal.fire({
+                title: 'Error!',
+                text: 'La consulta debe ser para una fecha posterior a la de hoy.',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
             todoOk = false;
         } else {
             todoOk = true;
         }
-        if (entradaDate < salidaDate) {
-            alert("La fecha de salida tiene que ser posterior a la de entrada.")
+        if (entradaLux <= salidaLux) {
+            Swal.fire({
+                title: 'Error!',
+                text: 'La fecha de salida tiene que ser posterior a la de entrada.',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
             todoOk = false;
         } else {
             todoOk = true;
         }
         if (mayores < 1) {
-            alert("Debe tener al menos un mayor a cargo.");
+            Swal.fire({
+                title: 'Error!',
+                text: 'Debe tener al menos un mayor a cargo.',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
             todoOk = false;
         } else {
             todoOk = true;
         }
         
         if (todoOk) {
-        
             localStorage.setItem('flname', flname);
             localStorage.setItem('email', email);
             localStorage.setItem('entrada', entrada);
@@ -61,5 +81,12 @@ document.getElementById('consulta').value = consulta;
             localStorage.setItem('mayores', mayores);
             localStorage.setItem('menores', menores);
             sessionStorage.setItem('consulta', consulta);
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Consulta enviada!',
+                showConfirmButton: false,
+                timer: 1500
+            });
         }
     }
